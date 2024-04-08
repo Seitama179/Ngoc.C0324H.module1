175f8bccf4870d74d70541589328817de3e58b29
Seitama179 <ngochd997@gmail.com>
2024-04-08 13:49:31 +0700

    up file bai tap form

--- ss6_JS_tong_quan/demo.js
+++ ss6_JS_tong_quan/demo.js
@@ -3,4 +3,4 @@ yourName = prompt('Nhap ten');
 console.log(yourName);
 alert(yourName);
 document.writeln("xin chào " + yourName);
-document.getElementById("demo1").innerHTML="Thứ 6 ngày 5 tháng 4";
+document.getElementById("today").innerHTML="Thứ 6 ngày 5 tháng 4";
